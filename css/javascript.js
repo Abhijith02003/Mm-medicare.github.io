2b5ac3f25007e32d670b137a5e7768251375b8a0
Abhijith02003 <abhijith02003@gmail.com>
2024-09-05 10:35:17 +0530

    Add files via upload

---
+++ css/javascript.js
@@ -0,0 +1,316 @@
+// script.js 
+function changeReadMore() { 
+	const mycontent = 
+		document.getElementById('mybox1id'); 
+	const mybutton = 
+		document.getElementById('mybuttonid'); 
+	const span1 = document.getElementById("span1") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+}
+
+function changeReadMore2() { 
+	const mycontent = 
+		document.getElementById('mybox1id2'); 
+	const mybutton = 
+		document.getElementById('mybuttonid2'); 
+	const span1 = document.getElementById("span2") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+} 
+
+function changeReadMore3() { 
+	const mycontent = 
+		document.getElementById('mybox1id3'); 
+	const mybutton = 
+		document.getElementById('mybuttonid3'); 
+	const span1 = document.getElementById("span3") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+} 
+
+function changeReadMore4() { 
+	const mycontent = 
+		document.getElementById('mybox1id4'); 
+	const mybutton = 
+		document.getElementById('mybuttonid4'); 
+	const span1 = document.getElementById("span4") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+} 
+
+function changeReadMore5() { 
+	const mycontent = 
+		document.getElementById('mybox1id5'); 
+	const mybutton = 
+		document.getElementById('mybuttonid5'); 
+	const span1 = document.getElementById("span5") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+} 
+
+function changeReadMore6() { 
+	const mycontent = 
+		document.getElementById('mybox1id6'); 
+	const mybutton = 
+		document.getElementById('mybuttonid6'); 
+	const span1 = document.getElementById("span6") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+}
+
+function changeReadMore7() { 
+	const mycontent = 
+		document.getElementById('mybox1id7'); 
+	const mybutton = 
+		document.getElementById('mybuttonid7'); 
+	const span1 = document.getElementById("span7") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+}
+
+function changeReadMore8() { 
+	const mycontent = 
+		document.getElementById('mybox1id8'); 
+	const mybutton = 
+		document.getElementById('mybuttonid8'); 
+	const span1 = document.getElementById("span8") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+}
+
+function changeReadMore9() { 
+	const mycontent = 
+		document.getElementById('mybox1id9'); 
+	const mybutton = 
+		document.getElementById('mybuttonid9'); 
+	const span1 = document.getElementById("span9") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+}
+function changeReadMore10() { 
+	const mycontent = 
+		document.getElementById('mybox1id10'); 
+	const mybutton = 
+		document.getElementById('mybuttonid10'); 
+	const span1 = document.getElementById("span10") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+}
+function changeReadMore11() { 
+	const mycontent = 
+		document.getElementById('mybox1id11'); 
+	const mybutton = 
+		document.getElementById('mybuttonid11'); 
+	const span1 = document.getElementById("span11") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+}
+function changeReadMore12() { 
+	const mycontent = 
+		document.getElementById('mybox1id12'); 
+	const mybutton = 
+		document.getElementById('mybuttonid12'); 
+	const span1 = document.getElementById("span12") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+}
+function changeReadMore13() { 
+	const mycontent = 
+		document.getElementById('mybox1id13'); 
+	const mybutton = 
+		document.getElementById('mybuttonid13'); 
+	const span1 = document.getElementById("span13") 
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		span1.style.display = "none"; 
+		mybutton.textContent = 'See Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+		mybutton.textContent = 'See More'; 
+		span1.style.display = "inline"; 
+	} 
+}
+
+// **************** ABOUTUS MODAL JS **********************
+
+
+// Get the modal
+
+var modalparent = document.getElementsByClassName("modal_multi");
+
+// Get the button that opens the modal
+
+var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
+
+// Get the <span> element that closes the modal
+var span_close_multi = document.getElementsByClassName("close_multi");
+
+// When the user clicks the button, open the modal
+function setDataIndex() {
+
+	for (i = 0; i < modal_btn_multi.length; i++)
+	{
+		modal_btn_multi[i].setAttribute('data-index', i);
+		modalparent[i].setAttribute('data-index', i);
+		span_close_multi[i].setAttribute('data-index', i);
+	}
+}
+
+
+
+for (i = 0; i < modal_btn_multi.length; i++)
+{
+	modal_btn_multi[i].onclick = function() {
+		var ElementIndex = this.getAttribute('data-index');
+		modalparent[ElementIndex].style.display = "block";
+	};
+
+	// When the user clicks on <span> (x), close the modal
+	span_close_multi[i].onclick = function() {
+		var ElementIndex = this.getAttribute('data-index');
+		modalparent[ElementIndex].style.display = "none";
+	};
+
+}
+
+window.onload = function() {
+
+	setDataIndex();
+};
+
+window.onclick = function(event) {
+	if (event.target === modalparent[event.target.getAttribute('data-index')]) {
+		modalparent[event.target.getAttribute('data-index')].style.display = "none";
+	}
+
+	// OLD CODE
+	if (event.target === modal) {
+		modal.style.display = "none";
+	}
+};
+
+//XXXXXXXXXXXXXXXXXXXXXXX    Modified old code    XXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Get the modal
+
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn2");
+
+// Get the <span> element that closes the modal
+var span = modal.getElementsByClassName("close")[0]; // Modified by dsones uk
+
+// When the user clicks on the button, open the modal
+
+btn.onclick = function() {
+
+	modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modal.style.display = "none";
+}
+
+
+
+
